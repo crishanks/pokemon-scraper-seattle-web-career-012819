da3acc3699ff6c993db0ff466e46dd167bd6f619
github username <crishanks12@gmail.com>
2019-02-07 14:09:29 -0800

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
 
     attr_accessor :id, :name, :type, :hp, :db
 
-    def initialize(id: 0, name: 'mew', type: 'magic', hp: nil, db: nil)
+    def initialize(id: nil, name: 'mew', type: 'magic', hp: nil, db: nil)
         @id = id
         @name = name
         @type = type
@@ -17,7 +17,7 @@ class Pokemon
     def self.find(id, db)
         rows = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
         row = rows.first
-        Pokemon.new(id: row[0], name: row[1], type: row[2])
+        Pokemon.new(id: row[0], name: row[1], type: row[2], hp: row[3])
     end
 
     # def alter_hp(id, hp:, db)
